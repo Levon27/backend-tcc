03f6bcdafaab39e0a2d1fb728ea319c89031400e
Levon <chahestian@hotmail.com>
2018-07-11 15:43:17 -0300

    rota para inserir dados por post agora sem tres requisicoes

--- public/index.php
+++ public/index.php
@@ -8,110 +8,13 @@ require '../vendor/autoload.php';
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
+
 $app = new \Slim\App;
 
 $app->map(['GET','POST'],'/hello/{name}', function (Request $request, Response $response, array $args) {
 	$nome = $args['name'];
 	
 	echo "Ola, $nome";
-});
-
-$app->map(['GET'],'/dados/{id}/{corrente}/{tensao}/{potencia}', function (Request $request, Response $response, array $args) {
-	
-	require_once("db.php");
-	
-	$req = array();
-	
-	$id = $request->getAttribute('id');
-	$c = $request->getAttribute('corrente');
-	$v = $request->getAttribute('tensao');
-	$p = $request->getAttribute('potencia');
-	
-	echo "id: $id, corrente: $c, tensão: $v, potência: $p \n";
-	
-	array_push($req,$id,$c,$v,$p);
-	
-	$query = $pdo->prepare('INSERT INTO teste_sustek VALUES (?,?,?,?)');
-	
-	if (empty($_SESSION["req1"])){
-			//first request
-			$_SESSION["req1"] = $req;
-			echo "primeira req";
-		} else if (empty($_SESSION["req2"])){
-			//second request
-			$_SESSION["req2"] = $req;
-			if ($_SESSION["req1"]==$_SESSION["req2"]){			
-				$query->execute($req);
-				echo "duas req iguais, inserir no banco";
-				session_destroy();
-				return $response->withStatus(201);
-			}
-		} else if (empty($_SESSION["req3"])){
-			//compare them here
-			$_SESSION["req3"] = $req;
-			
-			if ($_SESSION["req3"] == $_SESSION["req1"] || $_SESSION["req3"]==$_SESSION["req2"]){
-				$query->execute($req);
-				echo "requicao aceita, inserir no banco";
-				session_destroy();
-				return $response->withStatus(201);
-			}
-			
-			
-		}
-	
-});
-$app->map(['POST'],'/dados', function (Request $request, Response $response, array $args) {
-	
-	require_once("db.php");
-	
-	$req = array();
-	
-	$parsed_body = json_decode($request->getBody(),true);
-	
-	$id = $parsed_body['id_sensor'];
-	$c = $parsed_body['corrente'];
-	$v = $parsed_body['tensao'];
-	$p = $parsed_body['potencia'];
-	$h = $parsed_body['hora'];
-	$d = $parsed_body['data'];
-	
-	//$data_hora = " ' " . $d .' '. $h . " ' " ;
-	$data_hora =  $d .' '. $h;
-	
-	echo "id: $id, corrente: $c, tensão: $v, potência: $p data_hora:$data_hora \n";
-	
-	array_push($req,$id,$c,$v,$p,$data_hora);
-	
-	$query = $pdo->prepare('INSERT INTO teste_sustek (id_sensor,corrente,tensao,potencia,hora_medicao) VALUES (?,?,?,?,?)');
-	
-	if (empty($_SESSION["req1"])){
-			//first request
-			$_SESSION["req1"] = $req;
-			echo "primeira req";
-		} else if (empty($_SESSION["req2"])){
-			//second request
-			$_SESSION["req2"] = $req;
-			if ($_SESSION["req1"]==$_SESSION["req2"]){			
-				$query->execute($req);
-				echo "duas req iguais, inserir no banco";
-				session_destroy();
-				return $response->withStatus(201);
-			}
-		} else if (empty($_SESSION["req3"])){
-			//compare them here
-			$_SESSION["req3"] = $req;
-			
-			if ($_SESSION["req3"] == $_SESSION["req1"] || $_SESSION["req3"]==$_SESSION["req2"]){
-				$query->execute($req);
-				echo "requicao aceita, inserir no banco";
-				session_destroy();
-				return $response->withStatus(201);
-			}
-			
-			
-		}
-	
 });
 
 $app->map(['GET'],'/{id}/{corrente}/{tensao}/{potencia}', function (Request $request, Response $response, array $args){
@@ -133,6 +36,32 @@ $app->map(['GET'],'/{id}/{corrente}/{tensao}/{potencia}', function (Request $req
 	$query->execute($req);
 	
 		
+});
+$app->map(['POST'],'/dados', function (Request $request, Response $response, array $args) {
+	require_once("db.php");
+	
+	$req = array();
+	
+	$parsed_body = json_decode($request->getBody(),true);
+	
+	$id = $parsed_body['id_sensor'];
+	$c = $parsed_body['corrente'];
+	$v = $parsed_body['tensao'];
+	$p = $parsed_body['potencia'];
+	$h = $parsed_body['hora'];
+	$d = $parsed_body['data'];
+	
+	//$data_hora = " ' " . $d .' '. $h . " ' " ;
+	$data_hora =  $d .' '. $h;
+	
+	echo "id: $id, corrente: $c, tensão: $v, potência: $p data_hora:$data_hora \n";
+	
+	array_push($req,$id,$c,$v,$p,$data_hora);
+	
+	$query = $pdo->prepare('INSERT INTO teste_sustek (id_sensor,corrente,tensao,potencia,data_hora) VALUES (?,?,?,?,?)');
+	
+	$query->execute($req);
+	
 });
 
 $app->run();
